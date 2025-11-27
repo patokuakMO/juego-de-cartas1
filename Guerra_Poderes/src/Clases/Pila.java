f302e3a58c70985bb8ab8b40d6e92155897e5f77
Jhoan1316 <jhoandiegom@gmail.com>
2025-11-27 17:17:56 -0500

    primera

--- Guerra_Poderes/src/Clases/Pila.java
+++ Guerra_Poderes/src/Clases/Pila.java
@@ -1,8 +1,36 @@
 package Clases;
 /**
- *
+ *Pila simple para guardar cartas descartadas (LIFO).
  * @author Tilines
  */
 public class Pila {
     
+    private class NodoPila {
+        Carta carta;
+        NodoPila abajo;
+
+        public NodoPila(Carta carta) {
+            this.carta = carta;
+        }
+    }
+
+    private NodoPila tope;
+
+    public void push(Carta carta) {
+        NodoPila nuevo = new NodoPila(carta);
+        nuevo.abajo = tope;
+        tope = nuevo;
+    }
+
+    public Carta pop() {
+        if (vacia()) return null;
+        Carta c = tope.carta;
+        tope = tope.abajo;
+        return c;
+    }
+
+    public boolean vacia() {
+        return tope == null;
+    }
 }
+
