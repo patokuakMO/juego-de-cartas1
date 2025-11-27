ca814971fe227c6990f93849ed767d5b7bcae21c
Jhoan1316 <jhoandiegom@gmail.com>
2025-11-27 17:18:13 -0500

    primera tabla

--- Guerra_Poderes/src/Clases/TablaHash.java
+++ Guerra_Poderes/src/Clases/TablaHash.java
@@ -1,8 +1,19 @@
 package Clases;
+
+import java.util.HashMap;
+
 /**
- *
+ *Guarda estadísticas del jugador.
  * @author Tilines
  */
 public class TablaHash {
-    
+        private HashMap<String, Integer> datos = new HashMap<>();
+
+    public void setDato(String clave, Integer valor) {
+        datos.put(clave, valor);
+    }
+
+    public Integer getDato(String clave) {
+        return datos.getOrDefault(clave, 0);
+    }
 }
